e3255c3632f2c00eafbb05c572b2f96199d797d8
Julian Guo <kakugjl@gmail.com>
2025-11-21 17:22:57 +0900

    Fix flake8 formatting issues

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask
+
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def home():
-    return "Hello DevOps – Powered by Flask & Docker"
+    return "Hello, DevOps!"
+
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run()
